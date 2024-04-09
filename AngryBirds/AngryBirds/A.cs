db820eefd1ed83f3eee12275c26b5c51797395bd
EkaterinaOstapenko <145905865+EkaterinaOstapenko@users.noreply.github.com>
2024-04-09 11:02:44 +0500

    First

---
+++ AngryBirds/AngryBirds/A.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using 
+
+namespace AngryBirds
+{
+     class A
+    {
+        Application S = Application();
+        S.Run(new Window());
+    }
+}
